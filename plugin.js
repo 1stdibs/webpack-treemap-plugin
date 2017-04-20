1832b3ad12fa85842c20cd8be9bea7ff3e416b12
nameofname <nameofemail@gmail.com>
2017-04-20 16:24:32 -0400

    it bugs me when opn opens your tree map over and over and over.

--- plugin.js
+++ plugin.js
@@ -3,22 +3,26 @@ const templateFunc = require('lodash.template');
 const fs = require('fs');
 const path = require('path');
 const template = fs.readFileSync(path.resolve(__dirname, './page.template'), 'utf-8');
+let notCompiled = true;
 
 const opn = require('opn');
 const apply = (compiler) => {
-    compiler.plugin('done', (stats, callback) => {
-        const stj = stats.toJson();
-        const fileName = path.resolve(process.cwd(), 'wp-treemap.html');
-        fs.writeFile(fileName, templateFunc(template)({
-            stats: JSON.stringify(stj).replace(/</g, '\\u003c')
-                                    .replace(/-->/g, '--\\>')
-                                    .replace(/\u2028/g, '\\u2028')
-                                    .replace(/\u2029/g, '\\u2029'),
-            js: path.resolve(__dirname, './dist/bundle.js'),
-            stylesheet: path.resolve(__dirname, './dist/styles.css')
-        }), () => {
-            opn(fileName, {wait: false});
-        });
+    compiler.plugin('done', (stats) => {
+        if (notCompiled) {
+            const stj = stats.toJson();
+            const fileName = path.resolve(process.cwd(), 'wp-treemap.html');
+            fs.writeFile(fileName, templateFunc(template)({
+                stats: JSON.stringify(stj).replace(/</g, '\\u003c')
+                                        .replace(/-->/g, '--\\>')
+                                        .replace(/\u2028/g, '\\u2028')
+                                        .replace(/\u2029/g, '\\u2029'),
+                js: path.resolve(__dirname, './dist/bundle.js'),
+                stylesheet: path.resolve(__dirname, './dist/styles.css')
+            }), () => {
+                opn(fileName, {wait: false});
+                notCompiled = false;
+            });
+        }
     });
 };
 
